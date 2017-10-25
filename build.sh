9e4a3ed69c11959916a0cd1204e2860c1183c90e
“Oleg <helge.agr@gmail.com>
2017-10-25 15:34:25 +0200

    Migrating root project to the CMake build system;

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# reset the build directory
+rm -Rf ./build 
+mkdir -p build
+
+cd build 
+cmake .. -DCMAKE_INSTALL_PREFIX:PATH=/tmp/derivativesolver 
+make 
+make test
+#make install
+
--- src/MathParser/build.sh
+++ src/MathParser/build.sh
@@ -4,6 +4,7 @@
 rm -Rf ./build #clean the build directory
 mkdir build 
 cd build 
+#cmake .. -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX:PATH=/tmp/testlibagmathparser 
 cmake .. -DCMAKE_INSTALL_PREFIX:PATH=/tmp/testlibagmathparser 
 make 
 make install
