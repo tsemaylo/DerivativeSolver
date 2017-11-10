d82634ad243174ae8d0563b43010669fbc2b2b10
“Oleg <helge.agr@gmail.com>
2017-11-10 21:26:01 +0100

    Debug travis build #1;

--- travis.sh
+++ travis.sh
@@ -8,10 +8,10 @@ mkdir -p build
 
 cd build 
 cmake .. -DCMAKE_BUILD_TYPE=Debug
+cat src/MathParser/build/CMakeFiles/libagmathparser.dir/flags.make 
 make 
 make test
 
-cat src/MathParser/build/CMakeFiles/libagmathparser.dir/flags.make 
 
 if [ "${DO_CODE_ANALYSIS}" = "1" ]; then
     # do code check (not the best place for it, but it works)
