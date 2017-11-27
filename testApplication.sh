5ea86a3fb83e3aa9010d3cce9d8bae9436772a9c
“Oleg <helge.agr@gmail.com>
2017-11-27 13:26:48 +0100

    Creating application tests (initial set);

--- testApplication.sh
+++ testApplication.sh
@@ -1,5 +1,36 @@
 #/bin/sh
 
-echo "No acceptance tests are available."
+check(){
+   testId=$1
+   expected=$2
+   cmd=$3
+
+   actual=`$cmd`
+
+   if [ "$expected" == "$actual" ]; then
+      echo "$testId - OK"
+   else
+      echo "$testId - FAILED"
+      echo "   Expected: $expected"
+      echo "   Actual: $actual"
+   fi
+   
+}
+
+CMD=./DerivativeSolver
+
+echo "========================================"
+echo "Execitiong application tests."
+echo "----------------------------------------"
+
+check T01 "1" "$CMD x x" 
+check T02 "0" "$CMD x y" 
+check T03 "2*x" "$CMD x^2 x" 
+check T04 "x + 2*x" "$CMD \"2x + x^2\" y" 
+
+echo "========================================"
+
+# TODO consider all FAILED tests as errors
 
 exit 0
+
